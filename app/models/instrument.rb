c04794e7e80059ef1f605b71ab6c9c79dbc561b4
Irina Tarelkina <itarelkina@yahoo.ca>
2019-10-06 22:41:10 +0000

    progress in carts and line items

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -7,8 +7,8 @@ class Instrument < ApplicationRecord
   serialize :image, JSON # If you use SQLite, add this line
 
   validates :title, :brand, :price, :model, presence: true
-  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
-  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed. "}
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed. "}
   validates :price, length: { maximum: 7 }
 
   BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
